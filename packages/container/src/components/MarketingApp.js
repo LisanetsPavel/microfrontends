3040f92a97af641a271ce48ebd0115887dd2d81c
p.lysanets <p.lysanets@aimprosoft.com>
2023-02-17 18:08:07 +0200

    create routing

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,13 +1,24 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useEffect, useRef } from 'react';
+import { useHistory } from "react-router-dom";
 
 
 export default () => {
     const ref = useRef(null);
+    const history = useHistory();
 
     useEffect(() => {
-        mount(ref.current);
-    })
+       const { onNavigateParent } = mount(ref.current, {
+            onNavigate: ({ pathname: nextPathname }) => {
+                if (history.location.pathname !== nextPathname) {
+                    history.push(nextPathname);
+                }
+            }
+        });
+
+        history.listen(onNavigateParent);
+
+    }, [])
 
     return <div ref={ref} />
 }
